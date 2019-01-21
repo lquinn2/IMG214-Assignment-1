3d307ed5d0daeb78727e690de439dbbe01e0cb40
Student <student@cvs-r212d011.nic.bc.ca>
2019-01-21 15:38:23 -0800

    Assignment 2 90% completed

--- Assignment 1/ViewController.swift
+++ Assignment 1/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    enum UIKeyboardType : Int {
+        case namePhonePad = 6
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +27,27 @@ class ViewController: UIViewController {
     var secondNumber: Double = Double(Int(arc4random_uniform(10)))
     var operatorArray = ["*", "/", "+", "-"]
     var answer: Double = 0
-    var round: Int = 1
+    var round: Int = 0
     var score: Int = 0
-    var operatorInUse = "/"
+    var operatorInUse = "+"
     var correct: Bool = false
+    var level: Int = 0
+    var levels: [Int] = [3, 5, 10, 15]
+    var difficulty = 10
     
     func checkOperator(){
         if operatorInUse == "*" {
             answer = firstNumber * secondNumber
         } else if operatorInUse == "/" {
-            if secondNumber == 0 {
-                secondNumber += 1
-                secondLabel.text = String(secondNumber)
+            secondNumber = Double(Int(arc4random_uniform(10)))
+            if (firstNumber == 0.0) || (firstNumber == 3.0) || (firstNumber == 5.0) || (firstNumber == 7.0) || (firstNumber == 9.0) || (firstNumber == 11.0) || (firstNumber == 13.0) || (firstNumber == 15.0) || (firstNumber == 17.0) || (firstNumber == 19.0) {
+                firstNumber += 1
             }
+            if (secondNumber == 0.0) || (secondNumber == 3.0) || (secondNumber == 5.0) || (secondNumber == 7.0) || (secondNumber == 9.0) || (secondNumber == 11.0) || (secondNumber == 13.0) || (secondNumber == 15.0) || (secondNumber == 17.0) || (secondNumber == 19.0){
+                secondNumber += 1
+            }
+            self.firstLabel.text = String(Int(firstNumber)) + "!"
+            self.secondLabel.text = String(Int(secondNumber)) + "!"
             answer = firstNumber / secondNumber
         } else if operatorInUse == "+" {
             answer = firstNumber + secondNumber
@@ -48,14 +61,13 @@ class ViewController: UIViewController {
     func assignLabels(){
         self.firstLabel.text = String(Int(firstNumber))
         self.secondLabel.text = String(Int(secondNumber))
-        self.roundLabel.text = String(round)
+        self.levelLabel.text = String(level + 1)
         self.scoreLabel.text = String(score)
         self.inputField.text = ""
+        self.operatorLabel.text = String(operatorInUse)
     }
     
     @IBAction func showAlert() {
-        
-        checkOperator()
         
         var message = "Your answer was \(inputField.text!)"
 
@@ -69,19 +81,44 @@ class ViewController: UIViewController {
         if Double(answer) == Double(ans) {
             score += 5
             round += 1
-            operatorInUse = operatorArray[Int(arc4random_uniform(3))]
+            self.operatorInUse = operatorArray[Int(arc4random_uniform(3))]
+            title = "Correct"
             message += "\n You are CORRECT!"
-            firstNumber = Double(Int(arc4random_uniform(10)))
-            secondNumber = Double(Int(arc4random_uniform(10)))
+            firstNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+            secondNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+            checkOperator()
             assignLabels()
-            operatorInUse = operatorArray[Int(arc4random_uniform(3))]
-            self.operatorLabel.text = String(operatorInUse)
         } else {
+            round = 0
             message += "\n That is incorrect"
+            message += "\n Level progress has been reset"
             message += "\n Try again"
+            title = "Incorrect"
         }
         
-        let alert = UIAlertController(title: "Hello, World",
+        if round == levels[level] {
+            round = 0
+            message += "\n You have completed level \(level + 1)"
+            score += (level * 15)
+            if level != 3 {
+               level += 1
+            }
+            assignLabels()
+            switch level {
+            case 1:
+                difficulty = 10
+            case 2:
+                difficulty = 15
+            case 3:
+                difficulty = 20
+            case 4:
+                difficulty = 20
+            default:
+                difficulty = 10
+            }
+        }
+        
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         
@@ -105,13 +142,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var scoreLabel: UILabel!
     
-    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
 
     @IBAction func startOverButton() {
         score = 0
-        round = 1
-        firstNumber = Double(Int(arc4random_uniform(10)))
-        secondNumber = Double(Int(arc4random_uniform(10)))
+        round = 0
+        level = 0
+        firstNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+        secondNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+        assignLabels()
+    }
+    @IBAction func infoButton() {
+        let alert = UIAlertController(title: "New Question",
+                                      message: "Old answer was \(String(answer))",
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        firstNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+        secondNumber = Double(Int(arc4random_uniform(UInt32(difficulty))))
+        self.operatorInUse = operatorArray[Int(arc4random_uniform(3))]
+        checkOperator()
         assignLabels()
     }
 }
